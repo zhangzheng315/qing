302bac12e3ac8bb3ebefca87af6b32e8673e9631
neal <neal.tang@sicbon.com>
2021-02-02 20:06:19 +0800

    2.2合并冲突

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,16 +3,11 @@
 namespace app\index\controller;
 
 use think\Controller;
+
 class Index extends Controller
 {
-<<<<<<< HEAD
     public function index()
     {
         return $this->fetch();
     }
-=======
-   public function index() {
-       return $this -> fetch();
-   }
->>>>>>> 21e00f29b18bb94da7d910150b4f60865a215dde
 }
