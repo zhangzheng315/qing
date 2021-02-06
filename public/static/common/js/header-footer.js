f1a2704d1444c8748ba7fbe1aba9ec9762695c55
luca <1229339773@qq.com>
2021-02-06 17:46:29 +0800

    version2.6

---
+++ public/static/common/js/header-footer.js
@@ -0,0 +1,22 @@
+function init(){
+    $('.nav-pro-con').attr('real-height', $('.nav-pro-con').height());
+    $('.footer-about-item').attr('real-height', $('.footer-about-item').height());
+    $('.footer-service-item').attr('real-height', $('.footer-service-item').height());
+    $('.footer-industry-item').attr('real-height', $('.footer-industry-item').height());
+    $('.footer-scene-item').attr('real-height', $('.footer-scene-item').height());
+    $('.footer-college-item').attr('real-height', $('.footer-college-item').height());
+    $('.nav-pro-con').css({
+        'height': '0px'
+    })
+
+    $('.footer-item').css({
+        'height': '0px'
+    })
+}
+
+$(document).ready(function() {
+   const deviceWidth = $(window).width();
+   if(deviceWidth < 820) {
+       init();
+   }
+})
