6b0211e4949f9104956a59b4c0538b1827e9f7a4
zhangzheng <zhangzheng315324@163.com>
2021-03-05 09:35:08 +0800

    C:/Program Files/Git/

--- application/index/controller/Tutorial.php
+++ application/index/controller/Tutorial.php
@@ -2,12 +2,16 @@
 
 namespace app\index\controller;
 
+use app\admin\serve\VideoService;
 use think\Controller;
 
 class Tutorial extends Controller
 {
     public function index()
     {
-        return $this->fetch();
+        $data = request()->param();
+        $video_service = new VideoService();
+        $info = $video_service->videoInfo($data);
+        return $this->fetch('',compact('info'));
     }
 }
