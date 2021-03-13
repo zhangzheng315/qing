b89efe973e0bde6579c9a28e449af0b8332ca8e1
zhangzheng <zhangzheng315324@163.com>
2021-03-13 16:15:30 +0800

    C:/Program Files/Git/

--- application/index/controller/Industry.php
+++ application/index/controller/Industry.php
@@ -5,17 +5,25 @@ namespace app\index\controller;
 use app\admin\serve\BannerService;
 use app\admin\serve\SolutionService;
 use think\Controller;
+use think\Request;
 
 class Industry extends Controller
 {
+    public $banner_service;
+    public $solution_service;
+    public function __construct(Request $request = null)
+    {
+        parent::__construct($request);
+        $this->banner_service = new BannerService();
+        $this->solution_service = new SolutionService();
+    }
+
     public function index()
     {
-        $banner_service = new BannerService();
-        $solution_service = new SolutionService();
         $data = request()->param();
         $pid = isset($data['id']) ? $data['id'] : 3;
-        $banner_list = $banner_service->bannerListByPid($pid);
-        $solution_list = $solution_service->solutionList();
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        $solution_list = $this->solution_service->solutionList();
         return $this->fetch('',compact( 'banner_list','solution_list'));
     }
     /* 行业解决方案-医疗*/
--- application/index/controller/Solution.php
+++ application/index/controller/Solution.php
@@ -11,6 +11,7 @@ class Solution extends Controller
 {
     public $banner_service;
     public $solution_service;
+
     public function __construct(Request $request = null)
     {
         parent::__construct($request);
@@ -24,46 +25,78 @@ class Solution extends Controller
         $pid = isset($data['id']) ? $data['id'] : 3;
         $banner_list = $this->banner_service->bannerListByPid($pid);
         $solution_list = $this->solution_service->solutionList();
-        return $this->fetch('',compact( 'banner_list','solution_list'));
+        return $this->fetch('', compact('banner_list', 'solution_list'));
     }
+
     /* 场景解决方案-会议培训*/
     public function train()
     {
-      return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 31;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
+
     /* 场景解决方案-营销直播*/
     public function marketActivities()
     {
-        return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 35;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
+
     /* 场景解决方案-电商直播 */
     public function onlineRetailers()
     {
-        return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 36;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
+
     /* 场景解决方案-企业宣讲会*/
     public function enterprise()
     {
-      return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 37;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
+
     /* 场景解决方案-医学会议 */
     public function medical()
     {
-        return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 38;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
+
     /* 场景解决方案-手术示教*/
     public function operation()
     {
-      return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 39;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
+
     /* 场景解决方案-大会直播*/
     public function meetingLive()
     {
-        return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 40;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
+
     /* 场景解决方案-年会直播 */
     public function annualMeeting()
     {
-      return $this->fetch();
+        $data = request()->param();
+        $pid = isset($data['id']) ? $data['id'] : 41;
+        $banner_list = $this->banner_service->bannerListByPid($pid);
+        return $this->fetch('', compact('banner_list'));
     }
 }
