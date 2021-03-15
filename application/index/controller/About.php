7c6057741397ed2c1c1986dad8ee34b7ee3150ab
zhangzheng <zhangzheng315324@163.com>
2021-03-15 15:39:18 +0800

    C:/Program Files/Git/

--- application/index/controller/About.php
+++ application/index/controller/About.php
@@ -3,6 +3,7 @@
 namespace app\index\controller;
 
 use app\admin\serve\ContactService;
+use app\admin\serve\DevelopmentService;
 use app\admin\serve\JoinUsService;
 use think\Controller;
 
@@ -12,8 +13,10 @@ class About extends Controller
     {
         $joinUsService = new JoinUsService();
         $contactService = new ContactService();
+        $development_service = new DevelopmentService();
         $join_list = $joinUsService->joinUsList();
         $contact_list = $contactService->contactList();
-        return $this->fetch('',compact('join_list','contact_list'));
+        $development_list = $development_service->developmentList();
+        return $this->fetch('',compact('join_list','contact_list','development_list'));
     }
 }
