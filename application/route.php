c7f8eacd1bdabe14ef690da384e63d2093d31298
zhangzheng <zhangzheng315324@163.com>
2021-03-18 15:10:22 +0800

    C:/Program Files/Git/

--- application/route.php
+++ application/route.php
@@ -18,6 +18,7 @@ Route::rule('solution/medical', 'index/industry/medicalCare');      // 行业-�
 Route::rule('solution/education', 'index/industry/education');      // 行业-教育直播
 Route::rule('solution/banking', 'index/industry/banking');          // 行业-金融直播
 Route::rule('solution/media', 'index/industry/media');              // 行业-传媒直播
+
 Route::rule('solution/train', 'index/solution/train');              // 场景-会议培训
 Route::rule('solution/market', 'index/solution/marketActivities');  // 场景-营销直播
 Route::rule('solution/online', 'index/solution/onlineRetailers');   // 场景-电商直播
